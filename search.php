e3143c008095bc25293853af43683d4538073f9a
Louis Trinh <s3426686@student.rmit.edu.au>
2014-09-02 03:15:49 +1000

    l

--- search.php
+++ search.php
@@ -25,7 +25,7 @@ while ($row = mysql_fetch_assoc($result1)) {
 	}
 	
 	
-$queryYear = "SELECT DISTINCT * FROM wine ORDER BY year DESC;";
+$queryYear = "SELECT year * FROM wine ORDER BY year DESC;";
 $result = mysql_query($queryYear, $dbconn);
 $years = array();
 while ($row = mysql_fetch_assoc($result)) {
@@ -34,7 +34,7 @@ while ($row = mysql_fetch_assoc($result)) {
 	
 	
 //var_dump($regions);
-var_dump($years);
+//var_dump($years);
 
 ?>
 <!DOCTYPE HTML>
